4da0e12ff5a5d59fc847bff2f29f9d1aead0b95a
A.Hazzaa <ahazzaa@arabdt.com>
2024-01-13 12:56:09 +0200

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ahmed Hazzaa on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
